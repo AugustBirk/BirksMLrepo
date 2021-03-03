c2582c19dfb69cdf799808aa32975e6a57b6d895
AugustBirk <development@augustbirk.dk>
2021-03-03 14:47:52 +0100

    more

--- birksartificialdatalib.py
+++ birksartificialdatalib.py
@@ -7,29 +7,35 @@ def prob(p):
     r = random.random()
     return p > r
 
-def genData(length, y0=1000, vola=10):
+def genData(length, y0=1000, vola=0.1, statisize=1,
+            linearadd=1, expoadd=0.01, flashreduce=0.1,
+            dynamicflashchance=0.000001, flashchance=0.00001,
+            dynamic_flash_crash=True):
     arr = []
     for _ in range(length):
         # Statistic movements:
-        r = np.random.normal(loc=0, scale=vola)
-        s = 1
-        y0 += s*r
-
-        # Linear movement:
-        a = 1
-        if prob(0):
-            y0 += a
-
-        # Exponential movement:
-        ap = 0.1
-        bp = 0.0015
-        if prob(0.6):
-            y0 += a*np.exp(np.abs(bp*y0))
+        r = np.random.normal(loc=0, scale=y0*vola)
         
-        # Flash crash:
-        fl = 0.1
-        if prob(0.00001*y0):
-            y0 *= fl
+        if y0 > 0:
+            y0 += statisize*r
+
+            # Linear movement:
+            if prob(0.5):
+                y0 += linearadd
+
+            # Exponential movement:
+            if prob(0.06):
+                y0 *= 1 + expoadd
+
+            # Flash crash:
+            if dynamic_flash_crash:
+                if prob(dynamicflashchance*y0):
+                    y0 *= flashreduce
+            else:
+                if prob(flashchance):
+                    y0 *= flashreduce
+        else:
+            y0 = 0
 
         arr.append(y0)
     return arr
